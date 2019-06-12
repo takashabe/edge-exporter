1faef811a34bf81983cc22af75779bce1ffe92ac
takashabe <takashabe.io@gmail.com>
2019-06-12 10:14:07 +0900

    fix test

--- exporter_test.go
+++ exporter_test.go
@@ -19,15 +19,16 @@ func (*emptyExporter) ExportSpan(_ *trace.SpanData) {}
 
 func (*emptyExporter2) ExportSpan(_ *trace.SpanData) {}
 
-// TODO: internal test
-func TestExportersList(t *testing.T) {
+func TestRegisterAndUnregisterExporter(t *testing.T) {
 	edge := EdgeExporter{}
 	edge.RegisterExporter(&emptyExporter{})
 	edge.RegisterExporter(&emptyExporter{})
 	edge.RegisterExporter(&emptyExporter2{})
+  // TODO: internal test
 	assert.Equal(t, len(edge.exporters.Load()), 2)
 
   edge.UnregisterExporter(&emptyExporter{})
+  // TODO: internal test
 	assert.Equal(t, len(edge.exporters.Load()), 1)
 }
 
